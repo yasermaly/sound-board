610ab09433c727a8ff225ca47c83ac266d29278d
Yaser Aly <yaser-aly@live.com>
2023-10-20 13:14:53 -0700

    Sound board complete

--- script.js
+++ script.js
@@ -6,7 +6,20 @@ sounds.forEach((sound) => {
 
   btn.innerText = sound;
 
-  btn.addEventListener;
+  btn.addEventListener('click', () => {
+    stopSongs();
+
+    document.getElementById(sound).play();
+  });
 
   document.getElementById('buttons').appendChild(btn);
 });
+
+function stopSongs() {
+  sounds.forEach((sound) => {
+    const song = document.getElementById(sound);
+
+    song.pause();
+    song.currentTime = 0;
+  });
+}
